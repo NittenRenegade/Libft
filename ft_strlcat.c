0c609aabcc23dda506208e7f1b71a01ec83b2069
Cottage Skeleton <coskelet@il-d1.msk.21-school.ru>
2021-10-17 19:26:39 +0300

    ft_strlcat ver2

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: coskelet <coskelet@il-c2.msk.21-school.ru> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 20:32:04 by coskelet          #+#    #+#             */
-/*   Updated: 2021/10/17 17:47:35 by coskelet         ###   ########.fr       */
+/*   Updated: 2021/10/17 19:22:40 by coskelet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,22 @@
 
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-	int	src_len;
-	int	dst_len;
-	int	size;
-	int	pos;
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	size;
+	size_t	count;
 
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-	size = (int)dstsize;
+	size = dstsize;
 	if (size <= dst_len)
 		return (size + src_len);
 	if (size <= dst_len + 1)
 		return (size - 1 + src_len);
-	pos = dst_len;
-	while (pos < size && pos < (src_len + dst_len))
-	{
-		*(dst + pos) = *(src + pos);
-		pos++;
-	}
-	*(dst + pos) = '\0';
+	count = src_len;
+	if (size - dst_len < count)
+		count = size - dst_len - 1;
+	ft_memcpy(dst + dst_len, src, count);
+	*(dst + dst_len + count) = '\0';
 	return (dst_len + src_len);
 }
