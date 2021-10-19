3904906b581b1bcb9f66b3caa1ab4bfb701ffdb4
Cottage Skeleton <coskelet@il-j5.msk.21-school.ru>
2021-10-19 19:10:00 +0300

    ft_strncmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: coskelet <coskelet@il-c2.msk.21-school.ru> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 13:08:06 by coskelet          #+#    #+#             */
-/*   Updated: 2021/10/18 17:55:27 by coskelet         ###   ########.fr       */
+/*   Updated: 2021/10/19 17:36:44 by coskelet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,7 @@ int 	ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
